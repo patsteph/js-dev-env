817806ccdbb01bdd1a1106c7cef7c0c9153555af
patsteph <patsteph@cisco.com>
2017-08-16 20:12:09 -0500

    final pieces of course

---
+++ src/vendor.js
@@ -0,0 +1,6 @@
+/* This file contains reference to the vendor libraries we are using in this porject. This is used by webpack in the production build only. 
+*/
+
+/*eslint-disable no-unused-vars */
+
+import fetch from 'whatwg-fetch';
